ca1405567f10715f072d920d5c2cf449350eca9e
Rafael <rafaelangelowow@gmail.com>
2022-05-20 21:41:58 -0300

    remove devTolls

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,6 @@ const reducers = combineReducers({
 export const store = configureStore({
   reducer: reducers,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default reducers;
